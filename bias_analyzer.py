dc4688924b0edc4c6fb9ba0678a1893134f8bfa8
ibrahimboudaoud <ibrboudaoud@gmail.com>
2025-06-15 18:10:26 -0500

    Updated

--- bias_analyzer.py
+++ bias_analyzer.py
@@ -1,16 +1,25 @@
 from newspaper import Article
 import requests
+import re
+from transformers import pipeline
+import streamlit as st
+
+# UI
+st.title("Political Bias Detector")
+
+# NLP object
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
 # Extract Article object from valid URL
 def getURl() -> Article:
-    articleURL = input("Paste your article's URL: ")
+    articleURL = st.text_input("Paste your article's URL: ")
     notGot = True
     while(notGot):
         try:
             requests.get(articleURL)
             notGot = False
         except:
-            articleURL = input("Invalid URL, try again: ")
+            articleURL = st.text_input("Invalid URL, try again: ")
             notGot = True
     return Article(articleURL)
 
@@ -20,5 +29,10 @@ def getText(a : Article):
     a.parse()
     return a.text
 
-# Clean
-getURl()
+# Clean text
+def clean(txt : str) -> str:
+    re.sub(r'/s+', ' ', txt)
+    re.sub(r'[^\w\s]', '', txt)
+    return txt.lower()
+
+
